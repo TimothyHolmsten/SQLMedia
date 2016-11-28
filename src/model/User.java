82076fe26839408c8cce719c07636a230297f841
timothy <timhol@kth.se>
2016-11-28 22:23:22 +0100

    User things added

--- src/model/User.java
+++ src/model/User.java
@@ -1,7 +1,37 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  * Created by timothy on 2016-11-28.
  */
 public class User {
+    //user(userId, userName);
+    private int userId;
+    private String userName;
+    private ArrayList<Review> reviews;
+
+    public User(int userId, String userName) {
+        this.userId = userId;
+        this.userName = userName;
+        reviews = new ArrayList<>();
+    }
+
+    public User(int userId, String userName, ArrayList<Review> reviews) {
+        this.userId = userId;
+        this.userName = userName;
+        this.reviews = reviews;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public ArrayList<Review> getReviews() {
+        return (ArrayList<Review>) reviews.clone();
+    }
 }
