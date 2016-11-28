9342d76c2e2009da049fa74be2b48547d37672d5
timothy <timhol@kth.se>
2016-11-28 22:32:11 +0100

    Added media things

--- src/model/Media.java
+++ src/model/Media.java
@@ -4,4 +4,19 @@ package model;
  * Created by timothy on 2016-11-28.
  */
 public abstract class Media {
+    private int mediaId;
+    private String title;
+
+    public Media(int mediaId, String title) {
+        this.mediaId = mediaId;
+        this.title = title;
+    }
+
+    public int getMediaId() {
+        return mediaId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
