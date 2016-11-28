068f9387674a7fb587a1202cbe9defa0fa42b112
timothy <timhol@kth.se>
2016-11-28 22:14:58 +0100

    Review things added

--- src/model/Review.java
+++ src/model/Review.java
@@ -7,5 +7,28 @@ public class Review {
     //review( reviewId, reviewText,rating,mediaId, userId);
     private int reviewId;
     private String reviewText;
+    private int rating;
+    private int mediaId;
+    private int userId;
 
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getMediaId() {
+        return mediaId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public int getReviewId() {
+        return reviewId;
+    }
 }
