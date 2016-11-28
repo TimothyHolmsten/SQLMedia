8efbbd759bae59fc4a00d5fcc4e47b5a61ec529b
timothy <timhol@kth.se>
2016-11-28 22:45:50 +0100

    Added director things

--- src/model/Director.java
+++ src/model/Director.java
@@ -4,4 +4,20 @@ package model;
  * Created by timothy on 2016-11-28.
  */
 public class Director {
+    // Director(directorId,directorName);
+    private int directorId;
+    private int directorName;
+
+    public Director(int directorId, int directorName) {
+        this.directorId = directorId;
+        this.directorName = directorName;
+    }
+
+    public int getDirectorId() {
+        return directorId;
+    }
+
+    public int getDirectorName() {
+        return directorName;
+    }
 }
