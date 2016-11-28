28951b37ae3b0e97a2fadb5eb6b4213ad54a72ed
timothy <timhol@kth.se>
2016-11-28 22:45:36 +0100

    Added movie things

--- src/model/Movie.java
+++ src/model/Movie.java
@@ -3,15 +3,24 @@ package model;
 /**
  * Created by timothy on 2016-11-28.
  */
-public class Movie extends Media{
+public class Movie extends Media {
     // Movie(mediaId,, title, userId,directorId);
     private int directorId;
 
+    /**
+     * Using media as a superclass to get access to mediaId and title
+     * @param mediaId defines the movie
+     * @param title title of the movie
+     * @param directorId director of the movie
+     */
     public Movie(int mediaId, String title, int directorId) {
         super(mediaId, title);
         this.directorId = directorId;
     }
 
+    /**
+     * @return the director of the movie's id
+     */
     public int getDirectorId() {
         return directorId;
     }
