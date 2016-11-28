728e26f27d63321c5c114de0c5d19ea8b2bb1812
timothy <timhol@kth.se>
2016-11-28 22:31:59 +0100

    Added movie things

--- src/model/Movie.java
+++ src/model/Movie.java
@@ -3,5 +3,16 @@ package model;
 /**
  * Created by timothy on 2016-11-28.
  */
-public class Movie {
+public class Movie extends Media{
+    // Movie(mediaId,, title, userId,directorId);
+    private int directorId;
+
+    public Movie(int mediaId, String title, int directorId) {
+        super(mediaId, title);
+        this.directorId = directorId;
+    }
+
+    public int getDirectorId() {
+        return directorId;
+    }
 }
