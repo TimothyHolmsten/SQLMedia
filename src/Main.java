d7dd5fc4c3b7d4ed47deeba95ddd8a00b64482fe
douglas1410 <dpham@kth.se>
2016-11-28 23:22:40 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
 
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello awWorld");
+        primaryStage.setTitle("HelloWorld");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
