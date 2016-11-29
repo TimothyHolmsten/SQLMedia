e9a9e9307dc3f1f13a21a7ac5c8627e929b8a49b
timothy <timhol@kth.se>
2016-11-29 14:05:40 +0100

    Added things to album

--- src/model/Album.java
+++ src/model/Album.java
@@ -1,6 +1,25 @@
 package model;
+
+import java.util.ArrayList;
+
 /**
  * Created by timothy on 2016-11-28.
  */
-public class Album{
+public class Album extends Media {
+    // Album(mediaId,title,userId);
+    private ArrayList<Song> songs;
+
+    public Album(int mediaId, String title, int userId) {
+        super(mediaId, title, userId);
+        songs = new ArrayList<>();
+    }
+
+    public Album(int mediaId, String title, int userId, ArrayList<Song> songs) {
+        super(mediaId, title, userId);
+        this.songs = songs;
+    }
+
+    public ArrayList<Song> getSongs() {
+        return (ArrayList<Song>) songs.clone();
+    }
 }
